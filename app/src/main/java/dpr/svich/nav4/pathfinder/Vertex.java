ab82961ec0c3518c89e459fffea6bd19f7626aa3
EugenAl <zheka2142@gmail.com>
2019-11-03 21:24:37 +0300

    Add Vertex (#1)

---
+++ app/src/main/java/dpr/svich/nav4/pathfinder/Vertex.java
@@ -0,0 +1,32 @@
+package dpr.svich.nav4.pathfinder;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Вершина графа
+ */
+public class Vertex {
+
+    //  значение эвристической функции
+    private double heuristicFunction;
+
+    //  имя вершины
+    private String name;
+
+    //  идентификатор вершины
+    private int id;
+
+    //  стоимость пути
+    private double g;
+
+    //  список смежных вершин
+    private List<Vertex> adjacentVertex;
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        Vertex v = (Vertex) obj;
+        return (v != null && v.id == this.id && v.name.equals(this.name));
+    }
+}
